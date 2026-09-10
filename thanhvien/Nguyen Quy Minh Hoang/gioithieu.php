0b774bf85a8a3b22a2ebf17f4ab3ddc290c24fb0
Nguyen Quy Minh Hoang <nguyenquyminhhoang@gmail.com>
2026-09-10 22:14:50 +0700

    Them 2 tep cua Nguyen Quy Minh Hoang

---
+++ thanhvien/Nguyen Quy Minh Hoang/gioithieu.php
@@ -0,0 +1,19 @@
+<?php
+$hoten = "Nguyễn Qúy Minh Hoàng";            
+$lop = "24CNTT3";
+$sothich = "Cầu lông, đọc sách, lập trình web";
+?>
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+    <meta charset="UTF-8">
+    <title>Giới thiệu bản thân</title>
+</head>
+<body>
+    <h1>Tôi là <?php echo $hoten; ?> — lớp <?php echo $lop; ?></h1>
+
+    <p>Sở thích: <?php echo $sothich; ?></p>
+
+    <p>Trang được tạo lúc: <?php echo date("H:i:s d/m/Y"); ?></p>
+</body>
+</html>
